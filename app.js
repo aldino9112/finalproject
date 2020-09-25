148dd4dc49baf5b2f62d8a5edd75b722aa9b943c
aldino9112 <aldino.dian@gmail.com>
2020-09-25 08:15:48 +0700

    done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const session = require('express-session')
 
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended:true }))
